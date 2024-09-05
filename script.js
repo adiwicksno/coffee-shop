956830d1252038f689514c3f820b1d2f94f81b08
adiwicksno <adihadiwicaksono#gmail.com>
2024-09-05 09:37:48 +0700

    menambah ui search

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// Toggle class active
+// Toggle class active Hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu diklik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Toggle class active Search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
 };
 
 // Klik diluar sidebar untuk close nav
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
